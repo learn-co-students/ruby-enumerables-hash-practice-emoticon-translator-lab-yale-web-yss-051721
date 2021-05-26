9ec04cd6a8e127cab1700ed72b56d09c692795c2
clean-loop-a64b <dsjong2002@yahoo.com>
2021-05-26 09:12:45 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
 # require modules here
-
-def load_library
-  # code goes here
+require "yaml"
+def load_library(path)
+  orig = YAML.load_file(path)
+  ans = {}
+  orig.each do |name,arr|
+    ans[name] = {}
+    ans[name][:english] = arr[0]
+    ans[name][:japanese] = arr[1]
+  end
+  ans
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
   # code goes here
+  lib = load_library(path)
+  ans = lib.find do |name,vers|
+    vers[:english] == emoticon
+  end
+  ans ? ans[1][:japanese] : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+
+def get_english_meaning(path, emoticon)
   # code goes here
+  lib = load_library(path)
+  ans = lib.find do |name,vers|
+    vers[:japanese] == emoticon
+  end
+  ans ? ans[0] : "Sorry, that emoticon was not found"
 end
